b23ffe1ce22b6cae9edb2f1f97f97aa8e296e331
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-05 11:39:28 +0100

    Code reformatted by Fantomas

--- AOC2020/D04.fs
+++ AOC2020/D04.fs
@@ -1,99 +1,118 @@
 ﻿module D04
+
 open System.IO
 open System.Text.RegularExpressions
 
 
-type Passport = {
-    mutable BirthYear: Option<int>
-    mutable IssueYear: Option<int>
-    mutable ExpirYear: Option<int>
-    mutable Height: Option<string>
-    mutable HairColor: Option<string>
-    mutable EyeColor: Option<string>
-    mutable PassportID: Option<string>
-    mutable CountryID: Option<string>
-}
+type Passport =
+    { mutable BirthYear: Option<int>
+      mutable IssueYear: Option<int>
+      mutable ExpirYear: Option<int>
+      mutable Height: Option<string>
+      mutable HairColor: Option<string>
+      mutable EyeColor: Option<string>
+      mutable PassportID: Option<string>
+      mutable CountryID: Option<string> }
 
 let getInput filename =
     let input = File.ReadAllText(filename)
     let passportStrs = input.Split("\n\n")
 
-    let getPassport (passport:string) =
-        let mutable p = {
-            BirthYear = None;
-            IssueYear = None;
-            ExpirYear = None;
-            Height = None;
-            HairColor = None;
-            EyeColor = None;
-            PassportID = None;
-            CountryID = None;
-            }
-        passport.Split (' ', '\n') 
-        |> Array.iter (fun field ->
-            let kv = field.Split(':')
-            let kv' = (kv.[0], kv.[1])
-            match kv' with
-            | ("byr", v) -> p.BirthYear <- Some (int v)
-            | ("iyr", v) -> p.IssueYear <- Some (int v)
-            | ("eyr", v) -> p.ExpirYear <- Some (int v)
-            | ("hgt", v) -> p.Height <- Some v
-            | ("hcl", v) -> p.HairColor <- Some v
-            | ("ecl", v) -> p.EyeColor <- Some v
-            | ("pid", v) -> p.PassportID <- Some v
-            | ("cid", v) -> p.CountryID <- Some v
-            | _ -> ()
-            )
+    let getPassport (passport: string) =
+        let mutable p =
+            { BirthYear = None
+              IssueYear = None
+              ExpirYear = None
+              Height = None
+              HairColor = None
+              EyeColor = None
+              PassportID = None
+              CountryID = None }
+
+        passport.Split(' ', '\n')
+        |> Array.iter
+            (fun field ->
+                let kv = field.Split(':')
+                let kv' = (kv.[0], kv.[1])
+
+                match kv' with
+                | ("byr", v) -> p.BirthYear <- Some(int v)
+                | ("iyr", v) -> p.IssueYear <- Some(int v)
+                | ("eyr", v) -> p.ExpirYear <- Some(int v)
+                | ("hgt", v) -> p.Height <- Some v
+                | ("hcl", v) -> p.HairColor <- Some v
+                | ("ecl", v) -> p.EyeColor <- Some v
+                | ("pid", v) -> p.PassportID <- Some v
+                | ("cid", v) -> p.CountryID <- Some v
+                | _ -> ())
+
         p
 
     passportStrs |> Array.map getPassport
 
-let firstHalf () = 
+let firstHalf () =
     let passports = getInput "inputs/d04.txt"
     // printfn "%A" passports
-    passports 
-    |> Array.filter (fun p -> 
-        p.BirthYear.IsSome &&
-        p.IssueYear.IsSome &&
-        p.ExpirYear.IsSome &&
-        p.Height.IsSome &&
-        p.HairColor.IsSome &&
-        p.EyeColor.IsSome &&
-        p.PassportID.IsSome)
+    passports
+    |> Array.filter
+        (fun p ->
+            p.BirthYear.IsSome
+            && p.IssueYear.IsSome
+            && p.ExpirYear.IsSome
+            && p.Height.IsSome
+            && p.HairColor.IsSome
+            && p.EyeColor.IsSome
+            && p.PassportID.IsSome)
     |> Array.length
 
 let secondHalf () =
-    let inline (>=<) a (b,c) = a >= b && a<= c
-    let validHeight s = 
+    let inline (>=<) a (b, c) = a >= b && a <= c
+
+    let validHeight s =
         let len = String.length s
-        let um = s.[len - 2 ..]
+        let um = s.[len - 2..]
+
         match um with
-        | "cm" -> let h = int s.[.. len - 3] in h >=< (150,193) 
-        | "in" -> let h = int s.[.. len - 3] in h >=< (59,76)
+        | "cm" -> let h = int s.[..len - 3] in h >=< (150, 193)
+        | "in" -> let h = int s.[..len - 3] in h >=< (59, 76)
         | _ -> false
-    let validHairColor s = Regex.IsMatch (s, "\A#[\d|a-f][\d|a-f][\d|a-f][\d|a-f][\d|a-f][\d|a-f]\Z")
+
+    let validHairColor s =
+        Regex.IsMatch(s, "\A#[\d|a-f][\d|a-f][\d|a-f][\d|a-f][\d|a-f][\d|a-f]\Z")
+
     let validEyeColor s =
-        match s with 
-        | "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" -> true
+        match s with
+        | "amb"
+        | "blu"
+        | "brn"
+        | "gry"
+        | "grn"
+        | "hzl"
+        | "oth" -> true
         | _ -> false
-    let validPassportID s = Regex.IsMatch(s, "\A\d\d\d\d\d\d\d\d\d\Z")
+
+    let validPassportID s =
+        Regex.IsMatch(s, "\A\d\d\d\d\d\d\d\d\d\Z")
 
     let passports = getInput "inputs/d04.txt"
-    passports 
-    |> Array.filter (fun p -> 
-        p.BirthYear.IsSome &&
-        p.IssueYear.IsSome &&
-        p.ExpirYear.IsSome &&
-        p.Height.IsSome &&
-        p.HairColor.IsSome &&
-        p.EyeColor.IsSome &&
-        p.PassportID.IsSome)
-    |> Array.filter (fun p ->
-        p.BirthYear.Value >=< (1920,2002) &&
-        p.IssueYear.Value >=< (2010,2020) &&
-        p.ExpirYear.Value >=< (2020,2030) &&
-        validHeight p.Height.Value &&
-        validHairColor p.HairColor.Value &&
-        validEyeColor p.EyeColor.Value &&
-        validPassportID p.PassportID.Value)
+
+    passports
+    |> Array.filter
+        (fun p ->
+            p.BirthYear.IsSome
+            && p.IssueYear.IsSome
+            && p.ExpirYear.IsSome
+            && p.Height.IsSome
+            && p.HairColor.IsSome
+            && p.EyeColor.IsSome
+            && p.PassportID.IsSome)
+    |> Array.filter
+        (fun p ->
+            p.BirthYear.Value >=< (1920, 2002)
+            && p.IssueYear.Value >=< (2010, 2020)
+            && p.ExpirYear.Value >=< (2020, 2030)
+            && validHeight p.Height.Value
+            && validHairColor p.HairColor.Value
+            && validEyeColor p.EyeColor.Value
+            && validPassportID p.PassportID.Value)
     |> Array.length
--- AOC2020/D05.fs
+++ AOC2020/D05.fs
@@ -1,37 +1,64 @@
 ﻿module D05
+
 open System.IO
 
 let firstHalf () =
     let input = File.ReadAllLines("inputs/D05.txt")
-    let binaryPartions = input |> Array.map (fun l -> 
-        let rowBinPart = l.[..6].Replace('B','1').Replace('F','0') 
-        let colBinPart = l.[7..].Replace('R','1').Replace('L','0')
-        System.Convert.ToByte(rowBinPart,2), System.Convert.ToByte(colBinPart,2)
-        )
-    binaryPartions |> Array.map (fun (rowBinPart, colBinPart) ->
-        let row, col = rowBinPart &&& byte(127), colBinPart &&& byte(7) // find row and column of the seat
-        int(row) * 8 + int(col) // calculates seat ID
-        ) |> Array.max
-        
+
+    let binaryPartions =
+        input
+        |> Array.map
+            (fun l ->
+                let rowBinPart =
+                    l.[..6].Replace('B', '1').Replace('F', '0')
+
+                let colBinPart =
+                    l.[7..].Replace('R', '1').Replace('L', '0')
+
+                System.Convert.ToByte(rowBinPart, 2), System.Convert.ToByte(colBinPart, 2))
+
+    binaryPartions
+    |> Array.map
+        (fun (rowBinPart, colBinPart) ->
+            let row, col =
+                rowBinPart &&& byte (127), colBinPart &&& byte (7) // find row and column of the seat
+
+            int (row) * 8 + int (col))
+    |> Array.max
+
 
 let secondHalf () =
     let input = File.ReadAllLines("inputs/D05.txt")
-    let binaryPartions = input |> Array.map (fun l -> 
-        let rowBinPart = l.[..6].Replace('B','1').Replace('F','0') 
-        let colBinPart = l.[7..].Replace('R','1').Replace('L','0')
-        System.Convert.ToByte(rowBinPart,2), System.Convert.ToByte(colBinPart,2)
-        )
-    let sortedIDs = 
+
+    let binaryPartions =
+        input
+        |> Array.map
+            (fun l ->
+                let rowBinPart =
+                    l.[..6].Replace('B', '1').Replace('F', '0')
+
+                let colBinPart =
+                    l.[7..].Replace('R', '1').Replace('L', '0')
+
+                System.Convert.ToByte(rowBinPart, 2), System.Convert.ToByte(colBinPart, 2))
+
+    let sortedIDs =
         binaryPartions
-        |> Array.map (fun (rowBinPart, colBinPart) ->
-                let row, col = rowBinPart &&& byte(127), colBinPart &&& byte(7) // find row and column of the seat
-                int(row), int(col))
-        |> Array.filter (fun (row,_) -> row > 0 && row < 127) // removes very front and very back rows
-        |> Array.map (fun (row, col) -> row * 8 + col ) // calculates seat ID
-        |> Array.sort |> Array.indexed
-    sortedIDs 
-        |> Array.find (fun (i, id) -> // find missing ID
-        let _, nextID = sortedIDs.[i + 1]
-        nextID <> (id + 1) 
-        ) 
-        |> snd |> ((+) 1)
+        |> Array.map
+            (fun (rowBinPart, colBinPart) ->
+                let row, col =
+                    rowBinPart &&& byte (127), colBinPart &&& byte (7) // find row and column of the seat
+
+                int (row), int (col))
+        |> Array.filter (fun (row, _) -> row > 0 && row < 127) // removes very front and very back rows
+        |> Array.map (fun (row, col) -> row * 8 + col) // calculates seat ID
+        |> Array.sort
+        |> Array.indexed
+
+    sortedIDs
+    |> Array.find
+        (fun (i, id) -> // find missing ID
+            let _, nextID = sortedIDs.[i + 1]
+            nextID <> (id + 1))
+    |> snd
+    |> ((+) 1)
