75886f2de4901d25c5f0d5639e0ffd72f3b5e9f6
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-05 10:06:52 +0100

    Solution of the day 5

---
+++ AOC2020/D05.fs
@@ -0,0 +1,37 @@
+﻿module D05
+open System.IO
+
+let firstHalf () =
+    let input = File.ReadAllLines("inputs/D05.txt")
+    let binaryPartions = input |> Array.map (fun l -> 
+        let rowBinPart = l.[..6].Replace('B','1').Replace('F','0') 
+        let colBinPart = l.[7..].Replace('R','1').Replace('L','0')
+        System.Convert.ToByte(rowBinPart,2), System.Convert.ToByte(colBinPart,2)
+        )
+    binaryPartions |> Array.map (fun (rowBinPart, colBinPart) ->
+        let row, col = rowBinPart &&& byte(127), colBinPart &&& byte(7)
+        int(row) * 8 + int(col)
+        ) |> Array.max
+        
+
+let secondHalf () =
+    let input = File.ReadAllLines("inputs/D05.txt")
+    let binaryPartions = input |> Array.map (fun l -> 
+        let rowBinPart = l.[..6].Replace('B','1').Replace('F','0') 
+        let colBinPart = l.[7..].Replace('R','1').Replace('L','0')
+        System.Convert.ToByte(rowBinPart,2), System.Convert.ToByte(colBinPart,2)
+        )
+    let sortedIds = 
+        binaryPartions
+        |> Array.map (fun (rowBinPart, colBinPart) ->
+                let row, col = rowBinPart &&& byte(127), colBinPart &&& byte(7)
+                int(row), int(col))
+        |> Array.filter (fun (row,_) -> row > 0 && row < 127) 
+        |> Array.map (fun (row, col) -> row * 8 + col )
+        |> Array.sort |> Array.indexed
+    sortedIds 
+        |> Array.find (fun (i, id) -> 
+        let _, nextId = sortedIds.[i+1]
+        nextId <> (id + 1)
+        ) 
+        |> snd |> ((+) 1)
--- AOC2020/Program.fs
+++ AOC2020/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System.IO
 open D04
+open D05
 
 
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
@@ -12,7 +13,10 @@ let from whom =
 [<EntryPoint>]
 let main argv =
     printfn "Advent of Code\n   0xffff&2020\n"
-    printfn "--- Day 4: Passport Processing ---\nYour puzzle answer was %d\n" (D04.firstHalf  ()) 
+    printfn "--- Day 4: Passport Processing ---\nYour puzzle answer was %d" (D04.firstHalf  ()) 
     printfn "--- Part Two ---\nYour puzzle answer was %d\n\n" (D04.secondHalf ())
+    printfn "--- Day 5: Binary Boarding ---\nYour puzzle answer was %d" (D05.firstHalf  ()) 
+    printfn "--- Part Two ---\nYour puzzle answer was %d\n\n" (D05.secondHalf ())
+
 
     0 // return an integer exit code
